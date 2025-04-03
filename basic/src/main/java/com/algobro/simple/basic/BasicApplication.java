020d170020e215059d0f4ae403cd67269fe7dd9e
nagasani <nagasani.r@gmail.com>
2025-04-02 22:54:14 -0400

    Initial

--- basic/src/main/java/com/algobro/simple/basic/BasicApplication.java
+++ basic/src/main/java/com/algobro/simple/basic/BasicApplication.java
@@ -3,6 +3,22 @@ package com.algobro.simple.basic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/*
+ 🧩 Breakdown of what each does:
+@SpringBootConfiguration
+	A specialization of @Configuration.
+	Indicates that the class can be used by Spring's IoC container as a source of bean definitions.
+
+@EnableAutoConfiguration
+	Enables Spring Boot’s auto-configuration mechanism.
+	Automatically configures Spring and third-party libraries based on the classpath and defined beans.
+
+@ComponentScan
+	Scans for @Component, @Service, @Repository, @Controller, etc., starting from the package of the class where @SpringBootApplication is placed.
+	You can customize the base package with scanBasePackages.
+ */
+
 @SpringBootApplication
 public class BasicApplication {
 
